8b147ff075ed3b98b95989d7f9fd35e9f7e6fe6c
david <david_xd1996@live.com.mx>
2018-11-30 02:17:43 -0600

    trabajo con ficheros proyecto final

---
+++ fichero/fichero.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+FILE  *fichero;
+#define archivo "datos.txt"/*nombre del archivo*/
+#define MaxAlumnos 10/*limite de alumnos para el salon*/
+
+struct calificaciones{
+  int Actividades,/*40%*/
+      Examenes[2],/*15% c/u (2)*/
+      Participacion,/*10%*/
+      Departamental;/*20%*/
+}Promedio;
+
+struct datosAlumno{
+  char Nombre[50],/*se guarda el nombre de los alumnos*/
+      Codigo[10],/*se guarda el codigo de los alumnos*/
+      Carrera[10];/*se guarda la carrera de cada alumno*/
+      //Promedio_f Promedio;
+}Alumno;
+
+void crear() {/*esta funcion busca el fichero y si no existe crea uno*/
+  fichero = fopen(archivo, "rt");/*busca fichero para leerlo*/
+  if (fichero == NULL) {/*si ocurre algun erro lo crea*/
+    fichero = fopen(archivo, "wt");
+    printf("el fichero no existia y se creo uno");
+  }else{
+    printf("el fichero ya existe");
+  }
+}
+
+void nuevoAlumno(){
+  fflush(stdin);
+  printf("\nNombre: ");gets(Alumno.Nombre);
+  printf("\ncodigo: ");scanf("%s",&Alumno.Codigo);
+  printf("\ncarrera: ");scanf("%s",&Alumno.Carrera);
+  printf("\nActividades: ");scanf("%i",&Promedio.Actividades);
+  printf("\nexamen 1: ");scanf("%i",&Promedio.Examenes[0]);
+  printf("\nexamen 2: ");scanf("%i",&Promedio.Examenes[1]);
+  printf("\ndepartamental: ");scanf("%i",&Promedio.Departamental);
+}
+
+void agregarAlumno() {
+
+  fichero = fopen(archivo, "at");/*escribe al final del documento*/
+  fflush(stdin);
+  fwrite(Alumno.Nombre,1,strlen(Alumno.Nombre),fichero);
+  fprintf(fichero, "|");
+  fwrite(Alumno.Codigo,1,strlen(Alumno.Codigo),fichero);
+  fprintf(fichero, "|");
+  fwrite(Alumno.Carrera,1,strlen(Alumno.Carrera),fichero);
+  fprintf(fichero, "|\n");
+  // fwrite(Promedio.Actividades,1,strlen(Promedio.Actividades),fichero);
+  // fprintf(fichero, "|");
+  // fwrite(Promedio.Examenes[0],1,strlen(Promedio.Examenes[0]),fichero);
+  // fprintf(fichero, "|");
+  // fwrite(Promedio.Examenes[1],1,strlen(Promedio.Examenes[1]),fichero);
+  // fprintf(fichero, "|");
+  // fwrite(Promedio.Departamental,1,strlen(Promedio.Departamental),fichero);
+  fclose(fichero);
+}
+
+int main() {
+  crear();
+  nuevoAlumno();
+  agregarAlumno();
+  nuevoAlumno();
+  agregarAlumno();
+  nuevoAlumno();
+  agregarAlumno();
+
+
+
+  return 0;
+}
---
+++ pruebaF.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+// int Ieer_fichero () {
+//   FILE *fich;
+//   struct t-reg r;
+//   int er_dev = 0;
+//   if ((fich = fopen(“fichreg.dat", “rb")) == NULL) {
+//     printf ( "Error en apertura del fichero para lectura \n ");
+//     er_dev = 1.
+//   } else {
+//     fread (&r, sizeof(r), 1, fich);
+//     while (! feof(fich)) {
+//       printf ("%d: %s: %c\n", r.num, r.cad, r.car);
+//       fread (&r, sizeof(r), 1, fich);
+//     }
+//     fclose (fich);
+//   }
+//   return er_dev;
+// }
+
+
+
+// int main(){
+//   FILE *pf;
+//   int vals[3] = {1, 2, 3};
+//   // Abrimos fichero para escritura
+//   pf = fopen("datos.txt", "rt");
+//   //leer_fichero(pf);
+//   if (pf == NULL)
+//     {// Si el resultado es NULL mensaje de error
+//       printf("Error al abrir el fichero.\n");
+//       return -1;
+//     }
+//   else
+//     {// Si ha funcionado, comienza escritura
+//       fprintf(pf, "%i, %i, %i",
+// 	      vals[0], vals[1], vals[2]);
+//       printf("hola\n");
+//       fclose(pf); // Cerramos fichero
+//       return 0;
+//     }
+// }
+
+#define k "gfghhghghg"
+
+#include <stdio.h>
+
+int main() {
+
+
+
+  printf("%s", k);
+	return 0;
+}
